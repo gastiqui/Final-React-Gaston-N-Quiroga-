63e8e914262b822e076d2d0e26b4f9b417b42581
Gastón Quiroga <gasquiroga1@gmail.com>
2023-04-06 18:41:35 -0300

    proyecto final actualizado

--- src/components/ThankYou.jsx
+++ src/components/ThankYou.jsx
@@ -2,12 +2,11 @@ import { Link, useParams } from "react-router-dom";
 
 const ThankYou = () => {
     const { orderId } = useParams();
-
     return (
         <div className="container my-5">
             <div className="row">
                 <div className="col-md-12 text-center">
-                    {orderId ? <div class="alert alert-warning text-center" role="alert"><h3>¡Gracias por su Compra!</h3><p>Se generó su Orden de Compra con el ID: <b>{orderId}</b></p></div> : ""}
+                    {orderId ? <div class="alert alert-warning text-center" role="alert"><h3>¡Gracias por su compra!</h3><p>Se generó su órden de compra con el ID: <b>{orderId}</b></p></div> : ""}
                     <Link to={"/"} class="btn btn-warning">Volver al menú principal.</Link>
                 </div>
             </div>
